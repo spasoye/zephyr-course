86179ae9b8a31731cff8ddb5bf32f3ad30c9a670
Ivan <ivan@spasoye.com>
2026-04-08 13:37:53 +0200

    Fade demonstration implemented.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -16,6 +16,38 @@ static led_rgb pixel_struct = {.r = 0x00, .g = 0x00, .b = 0x00 }; // Blue, Red, 
 u_int32_t delay = CONFIG_BLINK_SLEEP_TIME_MS;
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+#if CONFIG_LED_RED
+    static struct led_rgb color = {.r = (0xFF), .g = (0x00), .b = (0x00)};
+#elif CONFIG_LED_GREEN
+    static struct led_rgb color = {.r = (0x00), .g = (0xFF), .b = (0x00)};
+#elif CONFIG_LED_BLUE
+    static struct led_rgb color = {.r = (0x00), .g = (0x00), .b = (0xFF)};
+#endif
+
+static struct led_rgb scale_brightness(struct led_rgb curr, uint8_t brightness)
+{
+    return (struct led_rgb){
+        .r = (curr.r * brightness) / 255,
+        .g = (curr.g * brightness) / 255,
+        .b = (curr.b * brightness) / 255
+    };
+}
+
+void fade_led()
+{
+    int step_delay = CONFIG_LED_FADE_MS / CONFIG_LED_BRIGHTNESS;
+
+    for (int i = 0; i < CONFIG_LED_BRIGHTNESS; i++) {
+        pixel_struct = scale_brightness(color, CONFIG_LED_BRIGHTNESS - i);
+        led_strip_update_rgb(led_strip, &pixel_struct, 1);
+        k_msleep(step_delay);
+    }
+
+    pixel_struct = (struct led_rgb){ .r = 0x00, .g = 0x00, .b = 0x00 };
+    led_strip_update_rgb(led_strip, &pixel_struct, 1);
+}
+
 int main(void)
 {
     int ret;
@@ -27,11 +59,21 @@ int main(void)
 
 
     while (1) {
-        // Set LED to Red
-        pixel_struct.r = 0x8F; // Red
-        pixel_struct.g = 0x00; // Green
-        pixel_struct.b = 0x00; // Blue
         
+#if CONFIG_ADV_LED
+
+        fade_led();
+#if CONFIG_LED_DEBUG
+        LOG_INF("LED faded out, going to sleep.\n");
+#endif
+        k_msleep(delay);
+#else      
+        /* Circle through Red, Green, and Blue colors with the configured brightness level. */
+
+        LOG_INF("LED brightness: %d %\n", CONFIG_LED_BRIGHTNESS);
+        // Set LED to Red
+        pixel_struct = scale_brightness((struct led_rgb){.r = 0x8F, .g = 0x00, .b = 0x00}, CONFIG_LED_BRIGHTNESS);
+        LOG_INF("RED value: %X\n", pixel_struct.r);
         ret = led_strip_update_rgb(led_strip, &pixel_struct, 1);
         LOG_INF("LED state: Red\n");
 
@@ -42,9 +84,7 @@ int main(void)
         k_msleep(delay);
 
         // Set LED to Green
-        pixel_struct.r = 0x00; // Red
-        pixel_struct.g = 0x8F; // Green
-        pixel_struct.b = 0x00; // Blue
+        pixel_struct = scale_brightness((struct led_rgb){.r = 0x00, .g = 0x87, .b = 0x00}, CONFIG_LED_BRIGHTNESS); 
 
         ret = led_strip_update_rgb(led_strip, &pixel_struct, 1);
         LOG_INF("LED state: Green\n");
@@ -55,9 +95,7 @@ int main(void)
         k_msleep(delay);
 
         // Set LED to Blue
-        pixel_struct.r = 0x00; // Red
-        pixel_struct.g = 0x00; // Green
-        pixel_struct.b = 0x8F; // Blue
+        pixel_struct = scale_brightness((struct led_rgb){.r = 0x00, .g = 0x00, .b = 0x8F}, CONFIG_LED_BRIGHTNESS);
 
         ret = led_strip_update_rgb(led_strip, &pixel_struct, 1);
         LOG_INF("LED state: Blue\n");
@@ -67,6 +105,7 @@ int main(void)
         }
 
         k_msleep(delay);
+#endif
     }
 
     return 0;
